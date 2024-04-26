7d16403413119a13da42c665aa5c2193deb4a2ae
jonathan gallego <jonathan.gallego7@gmail.com>
2024-04-26 11:55:29 -0500

    se agrega codigo faltante para visualizar usuarios front

--- src/api/user/user.request.js
+++ src/api/user/user.request.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 const api = 'http://localhost:3068/PE'
 
-export const getAllUsers = () => axios.get(`${api}/users/getAll`)
-export const getUser = (_id) => axios.get(`${api}/users/get/${_id}`);
-export const updateUser = (_id, userData) => axios.put(`${api}/users/modify/${_id}`, userData);
-export const ActivateAcc = (_id) => axios.get(`${api}/activation/${_id}`)
+export const getAllUsers = () => axios.get(`${api}/users/getAll`,{withCredentials: true})
+export const getUser = (_id) => axios.get(`${api}/users/get/${_id}`,{withCredentials: true});
+export const updateUser = (_id, userData) => axios.put(`${api}/users/modify/${_id}`, userData,{withCredentials: true});
+export const ActivateAcc = (_id) => axios.get(`${api}/activation/${_id}`,{withCredentials: true})
 
-export const deleteUser = (id) => axios.delete(`${api}/users/delete/${id}`);
+export const deleteUser = (id) => axios.delete(`${api}/users/delete/${id}`,{withCredentials: true});
 
-export const deleteUserConfirmation = (id, confirmationCode) => axios.delete(`${api}/users/delete/${id}/confirm`, { data: { confirmationCode } });
+export const deleteUserConfirmation = (id, confirmationCode) => axios.delete(`${api}/users/delete/${id}/confirm`, { data: { confirmationCode } },{withCredentials: true});
