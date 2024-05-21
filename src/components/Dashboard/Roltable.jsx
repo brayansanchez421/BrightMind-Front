9903d2527039beae6dec5e01b0429cc7a9934b01
andresherco21 <andresfelipehernandezcorredor@gmail.com>
2024-05-21 13:31:17 -0500

    Implementacion de creacion de categoria y lista de categorias

--- src/components/Dashboard/Roltable.jsx
+++ src/components/Dashboard/Roltable.jsx
@@ -202,6 +202,7 @@ const DataTable = () => {
         });
 
         setShowAssignModal(false);
+        window.location.reload();
       }
     } catch (error) {
       console.error("Error updating role:", error);
