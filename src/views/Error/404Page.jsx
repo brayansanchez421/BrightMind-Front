d22e05430e95b1c82fd79c4c08e954e925ac3c79
Juan David <jciro1360@gmail.com>
2024-05-29 15:41:13 -0500

    implementacion de createCategories

--- src/views/Error/404Page.jsx
+++ src/views/Error/404Page.jsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <img className="mx-auto h-12 w-auto animate-bounce" src={logo} alt="Logo" /> 
+          <img className="mx-auto h-48 w-auto animate-bounce" src={logo} alt="Logo" /> 
           <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-500">Ooops! Página no encontrada</h2> 
           <p className="mt-2 text-center text-sm text-gray-600">
             La página que estás buscando no existe. ¿Quieres volver a la página de inicio?
